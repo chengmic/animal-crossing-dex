e4f84bc43fffa3d95d534652f75b4ad645448c37
Michelle Cheng <chengmic@oregonstate.edu>
2024-09-04 13:15:08 -0700

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HeroSection from './components/HeroSection';
 import MainMenu from './components/MainMenu';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <HeroSection />
       <MainMenu />
     </BrowserRouter>
   );
---
+++ src/components/HeroSection.js
@@ -0,0 +1,12 @@
+import { Grid2 } from '@mui/material';
+
+function HeroSection() {
+  return (
+    <Grid2>
+        <h1>Welcome to Animal Crossing Dex</h1>
+        <h2>An encyclopedia based on the hit Nintendo franchise, Animal Crossing.</h2>
+    </Grid2>
+    );
+}
+
+export default HeroSection;
