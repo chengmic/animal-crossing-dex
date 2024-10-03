4d1ccbe95f1d0c16bbd83ae8b83050c83da39f2e
Michelle Cheng <chengmic@oregonstate.edu>
2024-10-03 16:05:12 -0700

    update info for bug card

--- src/components/BugCard.js
+++ src/components/BugCard.js
@@ -14,18 +14,36 @@ function BugCard() {
     <Grid2 container flexDirection={'column'}>
         {/*Back Button*/}
         <Grid2>
-        <button onClick={goBack}>Go Back</button>
+            <button onClick={goBack}>Go Back</button>
         </Grid2>
         
         {/*Name*/}
         <Grid2>
-        <h1>{bug.name}</h1>
+            <h1>{bug.name}</h1>
         </Grid2>
 
         {/*Image*/}
         <Grid2 size={2}>
-        <img src= {bug.render_url} alt="portrait" style={{maxWidth: "100%"}}></img>
+            <img src= {bug.render_url} alt="portrait" style={{maxWidth: "100%"}}></img>
         </Grid2>
+
+        <Grid2>
+            <p>Location: {bug.location}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Weather: {bug.weather}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Sell Price: {bug.sell_nook} bells</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Catchphrase: {bug.catchphrases}</p>
+        </Grid2>
+
+
     </Grid2>
     );
 }
