be40690ee7a5c2f711c0c495bbf8d40a73e0da5c
Michelle Cheng <chengmic@oregonstate.edu>
2024-10-03 15:19:28 -0700

    add new main menu icons

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,7 +1,15 @@
 import { Grid2 } from '@mui/material';
-import SampleImage from '../assets/images/sample.jpg';
 import { Link } from 'react-router-dom';
 import HeroSection from './HeroSection';
+
+import VillagerIcon from '../assets/images/villager_icon.png';
+import FishIcon from '../assets/images/fish_icon.png';
+import BugIcon from '../assets/images/bug_icon.png';
+import SeaCreatureIcon from '../assets/images/sea_creature_icon.png';
+import FossilIcon from '../assets/images/fossil_icon.png';
+import ArtworkIcon from '../assets/images/artwork_icon.png';
+
+
 
 function MainMenu() {
   return ( 
@@ -14,7 +22,7 @@ function MainMenu() {
                     style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}
                     state={{ objectType: 'villager' }}
                     >
-                        <img src={SampleImage} alt="sample" style={{maxWidth: "100%"}}></img>
+                        <img src={VillagerIcon} alt="sample" style={{maxWidth: "100%"}}></img>
                         <p style={{wordBreak: 'break-all'}}>Villagers</p>
                 </Link>
             </Grid2>
@@ -26,7 +34,7 @@ function MainMenu() {
                     style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}
                     state={{ objectType: 'fish' }}
                     >
-                        <img src={SampleImage} alt="sample" style={{maxWidth: "100%"}}></img>
+                        <img src={FishIcon} alt="sample" style={{maxWidth: "100%"}}></img>
                         <p style={{wordBreak: 'break-all'}}>Fish</p>
                 </Link>
             </Grid2>
@@ -38,7 +46,7 @@ function MainMenu() {
                     style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}
                     state={{ objectType: 'bug' }}
                     >
-                        <img src={SampleImage} alt="sample" style={{maxWidth: "100%"}}></img>
+                        <img src={BugIcon} alt="sample" style={{maxWidth: "100%"}}></img>
                         <p style={{wordBreak: 'break-all'}}>Bugs</p>
                 </Link>
             </Grid2>
@@ -50,7 +58,7 @@ function MainMenu() {
                     style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}
                     state={{ objectType: 'seaCreature' }}
                     >
-                        <img src={SampleImage} alt="sample" style={{maxWidth: "100%"}}></img>
+                        <img src={SeaCreatureIcon} alt="sample" style={{maxWidth: "100%"}}></img>
                         <p style={{wordBreak: 'break-all'}}>Sea Creatures</p>
                 </Link>
             </Grid2>
@@ -62,7 +70,7 @@ function MainMenu() {
                     style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}
                     state={{ objectType: 'fossil' }}
                     >
-                        <img src={SampleImage} alt="sample" style={{maxWidth: "100%"}}></img>
+                        <img src={FossilIcon} alt="sample" style={{maxWidth: "100%"}}></img>
                         <p style={{wordBreak: 'break-all'}}>Fossils</p>
                 </Link>
             </Grid2>
@@ -74,7 +82,7 @@ function MainMenu() {
                     style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}
                     state={{ objectType: 'artwork' }}
                     >
-                        <img src={SampleImage} alt="sample" style={{maxWidth: "100%"}}></img>
+                        <img src={ArtworkIcon} alt="sample" style={{maxWidth: "100%"}}></img>
                         <p style={{wordBreak: 'break-all'}}>Artworks</p>
                 </Link>
             </Grid2>
