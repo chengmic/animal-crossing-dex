b79bc0346164d270662f51d684d56ac7c2f66ca9
Michelle Cheng <chengmic@oregonstate.edu>
2024-10-03 15:51:10 -0700

    update artwork card info

--- src/components/ArtworkCard.js
+++ src/components/ArtworkCard.js
@@ -14,17 +14,49 @@ function ArtworkCard() {
     <Grid2 container flexDirection={'column'}>
         {/*Back Button*/}
         <Grid2>
-        <button onClick={goBack}>Go Back</button>
+            <button onClick={goBack}>Go Back</button>
         </Grid2>
         
         {/*Name*/}
         <Grid2>
-        <h1>{artwork.name}</h1>
+            <h1>{artwork.name}</h1>
         </Grid2>
 
         {/*Image*/}
         <Grid2 size={2}>
-        <img src= {artwork.image_url} alt="portrait" style={{maxWidth: "100%"}}></img>
+            <img src= {artwork.image_url} alt="portrait" style={{maxWidth: "100%"}}></img>
+        </Grid2>
+
+        <Grid2>
+            <p>Art Name: {artwork.art_name}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Author: {artwork.author}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Year: {artwork.year}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Type: {artwork.art_type}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Style: {artwork.art_style}</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Availability: {artwork.availability}</p>
+        </Grid2>
+        
+        <Grid2>
+            <p>Buy Price: {artwork.buy} bells</p>
+        </Grid2>
+
+        <Grid2>
+            <p>Sell Price: {artwork.sell} bells</p>
         </Grid2>
     </Grid2>
     );
