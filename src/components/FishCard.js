879bf3937bf559aa7fa8f886f594874457b5c0c3
Michelle Cheng <chengmic@oregonstate.edu>
2024-10-03 15:52:16 -0700

    code cleanup

--- src/components/FishCard.js
+++ src/components/FishCard.js
@@ -10,23 +10,23 @@ function FishCard() {
     const navigate = useNavigate();
     const goBack = () => navigate(-1);
 
-    
+
     /* Fish Card */
     return (
         <Grid2 container flexDirection={'column'}>
             {/*Back Button*/}
             <Grid2>
-            <button onClick={goBack}>Go Back</button>
+                <button onClick={goBack}>Go Back</button>
             </Grid2>
             
             {/*Fish Name*/}
             <Grid2>
-            <h1>{fish.name}</h1>
+                <h1>{fish.name}</h1>
             </Grid2>
 
             {/*Fish Image*/}
             <Grid2 size={2}>
-            <img src= {fish.render_url} alt="portrait" style={{maxWidth: "100%"}}></img>
+                <img src= {fish.render_url} alt="portrait" style={{maxWidth: "100%"}}></img>
             </Grid2>
 
             <Grid2>
