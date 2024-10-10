84704aa9fb84963e1282ef3a52a47c12ba5d1677
Michelle Cheng <chengmic@oregonstate.edu>
2024-10-10 15:53:30 -0700

    add container

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import './App.css';
 
-import MainMenu from './components/MainMenu';
-import VillagerCard from './components/VillagerCard';
-import ThumbnailsPage from './components/ThumbnailsPage';
-import FishCard from './components/FishCard';
-import BugCard from './components/BugCard';
-import SeaCreatureCard from './components/SeaCreatureCard';
-import FossilCard from './components/FossilCard';
-import ArtworkCard from './components/ArtworkCard';
-
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 
+import MainMenu from './components/MainMenu';
+import ThumbnailsPage from './components/ThumbnailsPage';
+import { Container } from '@mui/material';
+
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainMenu />} />
-        <Route path="/:objecttype" element={<ThumbnailsPage />} />
-        <Route path="/villagers/:name" element={<VillagerCard />} />
-        <Route path="/fish/:name" element={<FishCard />} />
-        <Route path="/bugs/:name" element={<BugCard />} />
-        <Route path="/seacreatures/:name" element={<SeaCreatureCard />} />
-        <Route path="/fossils/:name" element={<FossilCard />} />
-        <Route path="/artworks/:name" element={<ArtworkCard />} />
-      </Routes>
-
-    </BrowserRouter>
-
+    <div className='mainWallpaper'>
+      <Container className='background'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainMenu />} />
+            <Route path="/:objecttype" element={<ThumbnailsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </div>
   );
 }
 
