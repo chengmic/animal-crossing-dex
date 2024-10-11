d59104afc0b95d301b57333edbdc5db5fbf0f8d3
Michelle Cheng <chengmic@oregonstate.edu>
2024-10-10 20:17:14 -0700

    update villager card info

--- src/components/VillagerCard.js
+++ src/components/VillagerCard.js
@@ -14,45 +14,39 @@ function VillagerCard({villager}) {
             <img src= {villager.image_url} alt="portrait" style={{maxWidth: "100%"}}></img>
             </Grid2>
 
-            {/*Quick Info*/}
-            <Grid2 container spacing={4} style={{display:'flex', flexDirection:'horizontal' }}>
-                {/*Species*/}
-                <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
-                <p>Species</p>
-                <p>{villager.species}</p>
-                </Grid2>
+            {/*Species*/}
+            <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
+            <p>Species: </p>
+            <p>{villager.species}</p>
+            </Grid2>
 
-                {/*Type*/}
-                <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
-                <p>Personality</p>
-                <p>{villager.personality}</p>
-                </Grid2>
+            {/*Type*/}
+            <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
+            <p>Personality: </p>
+            <p>{villager.personality}</p>
+            </Grid2>
 
-                {/*Gender*/}
-                <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
-                <p>Gender</p>
-                <p>{villager.gender}</p>
-                </Grid2>
-
+            {/*Gender*/}
+            <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
+            <p>Gender: </p>
+            <p>{villager.gender}</p>
             </Grid2>
 
             {/*Birthday*/}
             <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
-            <p>Birthday</p>
+            <p>Birthday: </p>
             <p>{villager.birthday_month} {villager.birthday_day}</p>
-            <p>Sign</p>
-            <p>{villager.sign}</p>
             </Grid2>
 
             {/*Catchphrase*/}
             <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
-            <p>Catchphrase</p>
+            <p>Catchphrase: </p>
             <p>"{villager.phrase}"</p>
             </Grid2>
 
             {/*Quote*/}
             <Grid2 container spacing={1} style={{display:'flex', flexDirection:'horizontal' }}>
-            <p>Quote</p>
+            <p>Quote: </p>
             <p>"{villager.quote}"</p>
             </Grid2>
         </Grid2>
